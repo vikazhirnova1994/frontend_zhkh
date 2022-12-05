700d23fc391353696d177e7ae5bf3728be29d251
Vika Znirnova <vika.znirn@yandex.ru>
2022-12-05 17:41:39 +0500

    added reactive form for login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,36 +4,17 @@ import {HomeComponent} from "./home/home.component";
 import {AnalyticsComponent} from "./analytics/analytics.component";
 import {SettingComponent} from "./setting/setting.component";
 import {AboutComponent} from "./about/about.component";
-import {AppComponent} from "./app.component";
-
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: AppComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'analytics',
-    component: AnalyticsComponent,
-  },
-  {
-    path: 'setting',
-    component: SettingComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
-
- /* {
-    path: '**',
-    component: NotFoundComponent,
-  },*/
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+  { path: 'setting', component: SettingComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { HomeComponent } from './home/home.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
-import { SettingComponent } from './setting/setting.component';
-import { AboutComponent } from './about/about.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {SideNavComponent} from './side-nav/side-nav.component';
+import {HomeComponent} from './home/home.component';
+import {AnalyticsComponent} from './analytics/analytics.component';
+import {SettingComponent} from './setting/setting.component';
+import {AboutComponent} from './about/about.component';
 import {RouterOutlet} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -19,14 +23,20 @@ import {AppRoutingModule} from "./app-routing.module";
     HomeComponent,
     AnalyticsComponent,
     SettingComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent,
+    SignupComponent
   ],
-    imports: [
-        BrowserModule,
-        RouterOutlet,
-      AppRoutingModule
-    ],
+  imports: [
+    BrowserModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
